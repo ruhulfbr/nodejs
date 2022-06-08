7ed49bceb7016a73979022926d9f7eb8103bca7f
ruhultbd <ruhulamin@technobd.com>
2022-06-08 15:11:19 +0600

    math module

---
+++ math-module.js
@@ -0,0 +1,11 @@
+const add =  (a, b) => a+b
+const sub =  (a, b) => a-b
+const multipy =  (a, b) => a*b
+const division =  (a, b) => a/b
+
+// module.exports = {
+//     add,sub,multipy,division
+// }
+
+module.exports.add = add
+console.log(module.exports)
