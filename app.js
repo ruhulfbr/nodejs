c5a68013e44fec39763be19a470b84fbb40144a1
ruhultbd <ruhulamin@technobd.com>
2022-06-13 12:42:12 +0600

    making First Route

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res)=>{
+    console.log(req.query.hello); //To read query parameter
+    res.send('<h1>I am here</h1>')
+});
+
 app.listen(PORT, (err)=>{
     if(err){
         console.log(err)
