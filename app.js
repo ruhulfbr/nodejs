f08bafb243838365ce8629b439251f9dc5d9a56c
ruhultbd <ruhulamin@technobd.com>
2022-06-13 17:36:44 +0600

    Use morgan middleware

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const PORT = process.env.PORT || 8080
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+// app.use(morgan('dev));
 
-app.get('/contact', (req, res)=>{
+app.get('/contact', morgan('dev'), (req, res)=>{
     res.send('<h1>I am in Contact Page</h1>')
 });
 
@@ -17,7 +19,7 @@ app.get('/about', (req, res)=>{
 
 app.get('/', (req, res)=>{
     //console.log(req.query.hello); //To read query parameter
-    res.send('<h1>I am here</h1>')
+    res.send('<h1>I am in home page</h1>')
 });
 
 app.get('*', (req, res)=>{
