bb6113b829ff8951de3c3aaa4ddcac6334dc5ea8
ruhultbd <ruhulamin@technobd.com>
2022-06-13 12:50:04 +0600

    Create Multiple Page

--- app.js
+++ app.js
@@ -3,9 +3,26 @@ const express = require('express');
 
 const app = express();
 
+app.get('/contact', (req, res)=>{
+    res.send('<h1>I am in Contact Page</h1>')
+});
+
+app.get('/service', (req, res)=>{
+    res.send('<h1>I am in Seevice Page</h1>')
+});
+
+
+app.get('/about', (req, res)=>{
+    res.send('<h1>I am in about Page</h1>')
+});
+
 app.get('/', (req, res)=>{
-    console.log(req.query.hello); //To read query parameter
+    //console.log(req.query.hello); //To read query parameter
     res.send('<h1>I am here</h1>')
+});
+
+app.get('*', (req, res)=>{
+    res.send('<h1>404 page not found</h1>')
 });
 
 app.listen(PORT, (err)=>{
