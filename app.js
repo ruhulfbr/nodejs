bb16fcae97f5402039944179c34d9c1ba1780a85
ruhultbd <ruhulamin@technobd.com>
2022-06-18 23:39:01 +0600

    Insert data to database

--- app.js
+++ app.js
@@ -5,8 +5,30 @@ const mongoose = require('mongoose');
 const app = express();
 app.set('view engine', 'ejs')
 
+const Schema = mongoose.Schema;
+const postSchema = new Schema({
+    title: 'string',
+    status: 'string' 
+})
+
+let Post = mongoose.model('post', postSchema)
 
 app.get('/', (req, res)=>{
+
+    let newPost = new Post({
+        title: 'test title',
+        status: 'active'
+    })
+
+    newPost.save()
+    .then(()=>{
+        console.log('Data save to ther mongo DB database');
+    })
+    .catch((e)=>{
+        console.log(e);
+    })
+
+
     res.send('Work with mongo DB');
 });
 
